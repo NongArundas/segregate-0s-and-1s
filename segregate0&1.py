eaa681ce208d22efbc5a39cef6101f8b848f2330
Nongmaithem Arundas Singh <nongkakrun@gmail.com>
2024-02-18 20:09:07 +0530

    add Validation

--- segregate0&1.py
+++ segregate0&1.py
@@ -1,4 +1,10 @@
 def segregate_zeros_and_ones(arr):
+    # Add Validation
+    for num in arr:
+        if num != 0 and num != 1:
+            print("You are stuck in the part!")
+            return arr
+    
     # Count the number of zeros
     count_zeros = arr.count(0)
     
@@ -11,6 +17,6 @@ def segregate_zeros_and_ones(arr):
     return arr
 
 # Insert the input any integer size and enter "ENTER" to stop the array
-input_array = [int(item) for item in input().split()]
+input_array = [int(item) for item in input("Please enter only 0s and 1s in your list: \n").split()]
 output_array = segregate_zeros_and_ones(input_array)
 print(output_array)
